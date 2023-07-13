18ded51228d94c299a8569a1ebe591a7bb39b300
julimejia <davidmejiao43@gmail.com>
2023-07-12 22:49:41 -0500

    django base

--- eco_lisis/eco_lisis/settings.py
+++ eco_lisis/eco_lisis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j0ivsfs(x+h9&0%th25u_2exofis%^e%%*ia2pmq*$gkzafx4t'
+SECRET_KEY = 'django-insecure-w05svq#hnatj#ns-!csbgzgm1y-gzxs&@+ir1ju4iz0e=8#obg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
